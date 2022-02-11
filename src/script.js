2d0d6df9b2af28dc909a48b3c17fec2eeba8a23a
zaguimiola <98923174+zaguimiola@users.noreply.github.com>
2022-02-11 18:00:18 +0100

    fixed html, css and js
    remove and added some things

--- src/script.js
+++ src/script.js
@@ -62,18 +62,6 @@ function fahrenheit(event) {
   axios.get(apiUrl).then(showWeather);
 }
 
-function searchLocation(position) {
-  let apiKey = "13065ab195dea106282943aa8c990bea";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(showWeather);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
@@ -86,8 +74,5 @@ celsiusTemperature.addEventListener("click", celsius);
 
 let fahrenheitTemperature = document.querySelector("#fahrenheit");
 fahrenheitTemperature.addEventListener("click", fahrenheit);
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("New York");
