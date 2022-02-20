f0171b1419413e037fe41ab1e6a50c56e31fa16d
zaguimiola <98923174+zaguimiola@users.noreply.github.com>
2022-02-20 16:43:50 +0100

    added the search engine js

--- src/script.js
+++ src/script.js
@@ -45,8 +45,20 @@ function showWeather(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "13065ab195dea106282943aa8c990bea";
-let city = "Toulouse";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "13065ab195dea106282943aa8c990bea";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(showWeather);
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#search-text-input");
+  search(cityInputElement.value);
+}
+
+search("Toulouse");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
